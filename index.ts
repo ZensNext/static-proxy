56fcf30f54b6ec05da4442ec8c19dec7fbe41a51
Amagi:DDmxh <amagiddmxh@gmail.com>
2025-05-02 02:19:19 +0800

    Little tweaks

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { handle } from "hono/vercel";
-import app from "./app";
-import "dotenv/config";
-
-const handler = handle(app);
-
-export const GET = handler;
-export const OPTIONS = handler;
